508c358d4bd29fa493985dcd3fc6e67b3ff15faf
Mutyala Harshith <105526704+Mega-Hero@users.noreply.github.com>
2022-06-06 08:11:57 +0530

    Update extraction.py

--- tg_bot/modules/helper_funcs/extraction.py
+++ tg_bot/modules/helper_funcs/extraction.py
@@ -75,9 +75,9 @@ def extract_user_and_text(message: Message, args: List[str]) -> (Optional[int], 
         message.bot.get_chat(user_id)
     except BadRequest as excp:
         if excp.message in ("User_id_invalid", "Chat not found"):
-            message.reply_text("ഞാൻ ഇങ്ങനെയൊരാളെ ഇവിടെയെങ്ങും കണ്ടിട്ടേയില്ല... "
-                               "ഇയാളുടെ വാലോ തലയോ എന്തെങ്കിലും എനിക്ക് ഒന്ന് അയച്ചു താ (മെസ്സേജ് ആയാലും മതി)...  "
-                               "എന്നിട്ട് വേണം വേഗം പണി തുടങ്ങാൻ...")
+            message.reply_text("నేను గ్రూప్ నీ కనిపెట్ట లేక పోయినా... "
+                               "ఆ గ్రూప్ నుంచి ఒక మెసేజ్ పంపిస్తే చాలు (మెస్సేజ్ అయినా సరే)...  "
+                               "తరువాత నేను త్వరగా పని ప్రారంభిస్తాను...")
         else:
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
 
